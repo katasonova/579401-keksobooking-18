4301d4b30ef3a789ceebe0a80d5944e2dbc35ef5
Ekata <yogaelen108@gmail.com>
2019-09-23 20:50:51 +0100

    resolved all mentor's remarks

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 'use strict';
 
 var ADVERT_DATA = {
-  string: ['Лондон', 'Париж', 'Москва', 'Токио', 'Нью-Йорк', 'Рим', 'Барселона', 'Чикаго'],
-  type: ['palace', 'flat', 'house', 'bungalo'],
-  checkTime: ['12:00', '13:00', '14:00'],
-  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  photo: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-  maxPropertyPrice: 5000,
-  maxPropertyRooms: 15,
-  AMOUNT_OF_DISTINCT_AVATARS: 8,
-  Y_LOCATION: {
-    MIN: 130,
-    MAX: 630
-  }
+  STRING: ['Лондон', 'Париж', 'Москва', 'Токио', 'Нью-Йорк', 'Рим', 'Барселона', 'Чикаго'],
+  TYPE: ['palace', 'flat', 'house', 'bungalo'],
+  CHECK_TIME: ['12:00', '13:00', '14:00'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  PHOTO: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+  MAX_PROPERTY_PRICE: 5000,
+  MAX_PROPERTY_ROOMS: 15,
 };
 
 var LENGTH_OF_GENERATED_ARRAY = 8;
 var PIN_WIDTH = 40;
+var Y_LOCATION = {
+  MIN: 130,
+  MAX: 630
+};
+var MIN_NUMBER = 1;
 
 var mapsWidth = document.querySelector('.map').offsetWidth;
 var pinsList = document.querySelector('.map__pins');
@@ -26,72 +26,67 @@ var getRandomArrayElement = function (array) {
   return Math.round(Math.random() * (array.length - 1));
 };
 
-var getRandomNumber = function (maxNumber) {
-  return Math.ceil(Math.random() * maxNumber);
-};
-
-// no idea how to make the number without random()
-var getAvatarNumber = function () {
-  return '0' + Math.ceil(Math.random() * ADVERT_DATA.AMOUNT_OF_DISTINCT_AVATARS);
-};
-
-var getRandomArray = function (array) {
-  var newArray = [];
-
-  for (var i = 0; i < getRandomNumber(array.length); i++) {
-    newArray.push(array[i]);
-  }
-
-  return newArray;
-};
-
 var getRandomNumberWithinRange = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var generateAnAuthor = function () {
-  return {
-    avatar: 'img/avatars/user' + getAvatarNumber() + '.png'
-  };
+var shuffleArray = function (array) {
+  var elementsInArray = array.length;
+  var temp;
+  var index;
+
+  while (elementsInArray > 0) {
+    index = Math.floor(Math.random() * elementsInArray);
+    elementsInArray--;
+    temp = array[elementsInArray];
+    array[elementsInArray] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
 };
 
-var generateLocation = function (advertData) {
-  return {
-    x: getRandomNumberWithinRange(PIN_WIDTH / 2, mapsWidth - PIN_WIDTH).toString(),
-    y: getRandomNumberWithinRange(advertData.Y_LOCATION.MIN, advertData.Y_LOCATION.MAX).toString()
-  };
+var getRandomArray = function (array) {
+  var newArray = [];
+
+  for (var i = 0; i < getRandomNumberWithinRange(MIN_NUMBER, array.length); i++) {
+    newArray.push(array[i]);
+  }
+
+  return shuffleArray(newArray);
 };
 
-var generateAnOffer = function (advertData, locationObj) {
+var generateAdvert = function (index) {
   return {
-    title: advertData.string[getRandomArrayElement(advertData.string)],
-    address: locationObj.x + ', ' + locationObj.y,
-    price: getRandomNumber(advertData.maxPropertyPrice),
-    type: advertData.type[getRandomArrayElement(advertData.type)],
-    rooms: getRandomNumber(advertData.maxPropertyRooms),
-    checkin: advertData.checkTime[getRandomArrayElement(advertData.checkTime)],
-    checkout: advertData.checkTime[getRandomArrayElement(advertData.checkTime)],
-    features: getRandomArray(advertData.features),
-    description: advertData.string[getRandomArrayElement(advertData.string)],
-    photos: getRandomArray(advertData.photo)
-  };
-};
-
-var generateOneAdvert = function (advertData, locationObj) {
-  return {
-    author: generateAnAuthor(),
-    offer: generateAnOffer(advertData, locationObj),
-    location: generateLocation(advertData)
+    author: {
+      avatar: 'img/avatars/user' + index + '.png'
+    },
+    offer: {
+      title: ADVERT_DATA.STRING[getRandomArrayElement(ADVERT_DATA.STRING)],
+      address: location.x + ', ' + location.y,
+      price: getRandomNumberWithinRange(MIN_NUMBER, ADVERT_DATA.MAX_PROPERTY_PRICE),
+      type: ADVERT_DATA.TYPE[getRandomArrayElement(ADVERT_DATA.TYPE)],
+      rooms: getRandomNumberWithinRange(MIN_NUMBER, ADVERT_DATA.MAX_PROPERTY_ROOMS),
+      checkin: ADVERT_DATA.CHECK_TIME[getRandomArrayElement(ADVERT_DATA.CHECK_TIME)],
+      checkout: ADVERT_DATA.CHECK_TIME[getRandomArrayElement(ADVERT_DATA.CHECK_TIME)],
+      features: getRandomArray(ADVERT_DATA.FEATURES),
+      description: ADVERT_DATA.STRING[getRandomArrayElement(ADVERT_DATA.STRING)],
+      photos: getRandomArray(ADVERT_DATA.PHOTO)
+    },
+    location: {
+      x: getRandomNumberWithinRange(PIN_WIDTH / 2, mapsWidth - PIN_WIDTH).toString(),
+      y: getRandomNumberWithinRange(Y_LOCATION.MIN, Y_LOCATION.MAX).toString()
+    }
   };
 };
 
 var generateAdvertsList = function () {
   var generatedAdverts = [];
 
-  while (generatedAdverts.length < LENGTH_OF_GENERATED_ARRAY) {
-    generatedAdverts.push(generateOneAdvert(ADVERT_DATA, generateLocation(ADVERT_DATA)));
+  for (var i = 0; i < LENGTH_OF_GENERATED_ARRAY; i++) {
+    generatedAdverts.push(generateAdvert(i));
   }
 
   return generatedAdverts;
