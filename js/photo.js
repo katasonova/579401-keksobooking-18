935bdde23ee45661722522178ec1c96935aa858d
Ekata <yogaelen108@gmail.com>
2019-11-14 23:58:27 +0000

    implemented multiphoto load

--- js/photo.js
+++ js/photo.js
@@ -1,35 +1,41 @@
 'use strict';
 
-(function() {
+(function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var avatarChooser = document.querySelector('.ad-form__field input[type=file]');
   var avatarPreview = document.querySelector('.ad-form-header__preview img');
+  var defaultAvatar = avatarPreview.src;
 
+  // var container = document.querySelector('.ad-form__photo-container');
   var propertyImgChooser = document.querySelector('.ad-form__upload input[type=file]');
+  var propertyImg = document.querySelector('.ad-form__photo');
+  propertyImg.style.display = 'flex';
 
-  var createImg = function() {
-    var propertyImg = document.querySelector('.ad-form__photo');
+
+  // container.style.maxWidth = '450px';
+  // container.style.maxHeight = '250px';
+
+  propertyImgChooser.multiple = 'multiple';
+
+  var createImg = function (src) {
     var img = document.createElement('img');
-    //border-color:White; border-style:none; border-width:0;
-    //img.src = '';
+    img.src = src;
     img.width = 70;
     img.height = 70;
-    // img.style.borderColor = 'white';
-    // img.style.borderStyle = 'none';
-    // img.style.borderWidth = '0';
     img.style.borderRadius = '5px';
+    img.style.marginRight = '5px';
     propertyImg.appendChild(img);
 
     var propertyImgPreview = document.querySelector('.ad-form__photo img');
     return propertyImgPreview;
-  }
+  };
 
-  var uploadImg = function(input, img) {
-      return function () {
-    var file = input.files[0];
+  var uploadImg = function (input, img) {
+    return function () {
+      var file = input.files[0];
 
-    if (file) {
+      if (file) {
         var fileName = file.name.toLowerCase();
 
         var matches = FILE_TYPES.some(function (expansion) {
@@ -37,18 +43,55 @@
         });
 
         if (matches) {
+          var reader = new FileReader();
+
+          reader.addEventListener('load', function () {
+            img.src = reader.result;
+          });
+
+          reader.readAsDataURL(file);
+        }
+      }
+    };
+  };
+
+  var uploadImgs = function (input) {
+    return function () {
+      var files = Array.from(input.files);
+
+      if (files) {
+        var matches = files.every(function (img) {
+          var fileName = img.name.toLowerCase();
+          return FILE_TYPES.some(function (expansion) {
+            return fileName.endsWith(expansion);
+          });
+        });
+
+        if (matches) {
+          files.forEach(function (file) {
             var reader = new FileReader();
 
             reader.addEventListener('load', function () {
-                img.src = reader.result;
+            // propertyImg.style.maxWidth = '400px';
+            // propertyImg.style.maxHeight = '200px';
+              createImg(reader.result);
             });
 
             reader.readAsDataURL(file);
+          });
+
         }
+      }
     };
-}
-};
+  };
 
   avatarChooser.addEventListener('change', uploadImg(avatarChooser, avatarPreview));
-  propertyImgChooser.addEventListener('change', uploadImg(propertyImgChooser, createImg()));
+  propertyImgChooser.addEventListener('change', uploadImgs(propertyImgChooser));
+
+  window.photo = {
+    remove: function () {
+      avatarPreview.src = defaultAvatar;
+      propertyImg.innerHTML = '';
+    }
+  };
 })();
